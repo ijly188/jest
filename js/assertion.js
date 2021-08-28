8c7ffc68a3c5b7e2b3eefb94ce05915e48080616
terry.yao <RT006469@testritegroup.com>
2021-08-28 21:06:26 +0800

    jestNewVersion

---
+++ js/assertion.js
@@ -0,0 +1,62 @@
+const { sum, subtract } = require('./calculation');
+const { test } = require('./framework');
+
+// let result, expected;
+
+// result = sum(8, 7);
+// expected = 15;
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`);
+// }
+
+// result = subtract(8, 7);
+// expected = 1;
+// if (result !== expected) {
+//   throw new Error(`${result} is not equal to ${expected}`);
+// }
+
+// const result = sum(8, 7)
+// const expected = 15
+// expect(result).toBe(expected) // 變成易讀的 assertion
+
+function expect(result) {
+  return {
+    toBe(expected) {
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`)
+      }
+    },
+  }
+}
+
+// 同步區
+test('同步區 add numbers', () => {
+  const result = sum(8, 7)
+  const expected = 15
+  expect(result).toBe(expected)
+})
+
+test('同步區 subtract numbers', () => {
+  const result = subtract(8, 7)
+  const expected = 1
+  expect(result).toBe(expected)
+})
+
+// 非同步區
+test('非同步區 add numbers', async () => {
+  const result = await sum(8, 7)
+  const expected = 15
+  expect(result).toBe(expected)
+})
+test('非同步區 subtract numbers', async () => {
+  const result = await subtract(8, 7)
+  const expected = 1
+  expect(result).toBe(expected)
+})
+
+
+
+
+module.exports = {
+  expect,
+}
---
+++ js/framework.js
@@ -0,0 +1,13 @@
+async function test(title, callback) {
+  try {
+    await callback()
+    console.log(`✅ ${title}`)
+  } catch (error) {
+    console.error(`❌ ${title}`)
+    console.error(error)
+  }
+}
+
+module.exports = {
+  test,
+}
